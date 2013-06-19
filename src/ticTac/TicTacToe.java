6892ded6609ff999f62433f2b23a9fda621ed03b
hachteha448 <hachteha448@gmail.com>
2013-06-20 00:04:04 +0300

    Update TicTacToe.java

--- src/ticTac/TicTacToe.java
+++ src/ticTac/TicTacToe.java
@@ -5,18 +5,15 @@ import java.awt.event.*;
 
 
 public class TicTacToe implements ActionListener {
-    final String VERSION = "1.0";
+    final String VERSION = "0.7";
     JFrame window = new JFrame("Крестики-нолики" + VERSION);
 
     JMenuBar mnuMain = new JMenuBar();
     JMenuItem mnuNewGame = new JMenuItem("Новая игра"),
-            mnuInstruction = new JMenuItem("Инструкция"),
-            mnuExit = new JMenuItem("Выход"),
-            mnuAbout = new JMenuItem("О игре");
+            mnuExit = new JMenuItem("Выход");
 
-    JButton btn1v1 = new JButton("Player vs Player"),
-            btn1vCPU = new JButton("Player vs CPU"),
-            btnBack = new JButton("<--back");
+    JButton btn1v1 = new JButton("Начать новую игру друг против друга"),
+            btnBack = new JButton("<--назад");
     JButton btnEmpty[] = new JButton[10];
 
     JPanel pnlNewGame = new JPanel(),
@@ -25,14 +22,14 @@ public class TicTacToe implements ActionListener {
             pnlTop = new JPanel(),
             pnlBottom = new JPanel(),
             pnlPlayingField = new JPanel();
-    JLabel lblTitle = new JLabel("Tic-Tac-Toe");
+    JLabel lblTitle = new JLabel("Крестики-Нолики");
     JTextArea txtMessage = new JTextArea();
 
+    //различные возможные выигрышные комбинации по горизонтали, вертикали и диагонали
     final int winCombo[][] = new int[][] {
             {1, 2, 3}, {1, 4, 7}, {1, 5, 9},
             {4, 5, 6}, {2, 5, 8}, {3, 5, 7},
             {7, 8, 9}, {3, 6, 9}
-/*Horizontal Wins*/ /*Vertical Wins*/ /*Diagonal Wins*/
     };
     final int X = 412, Y = 268, color = 190;
     boolean inGame = false;
@@ -42,15 +39,16 @@ public class TicTacToe implements ActionListener {
     int turn = 1;
     int wonNumber1 = 1, wonNumber2 = 1, wonNumber3 = 1;
 
-    public TicTacToe() { //Setting game properties and layout and sytle...
-//Setting window properties:
+    //установка начальных настроек игры и вывода компонентов
+    public TicTacToe() {
+        //настройка окна
         window.setSize(X, Y);
         window.setLocation(450, 260);
         window.setResizable(false);
         window.setLayout(new BorderLayout());
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-//Setting Panel layouts and properties
+        //установка свойств менеджеров расположения
         pnlNewGame.setLayout(new GridLayout(2, 1, 2, 10));
         pnlNorth.setLayout(new FlowLayout(FlowLayout.CENTER));
         pnlSouth.setLayout(new FlowLayout(FlowLayout.CENTER));
@@ -65,26 +63,20 @@ public class TicTacToe implements ActionListener {
         pnlBottom.setLayout(new FlowLayout(FlowLayout.CENTER));
         pnlNewGame.setBackground(Color.blue);
 
-//Adding menu items to menu bar
+        //добавление пунктов меню
         mnuMain.add(mnuNewGame);
-        mnuMain.add(mnuInstruction);
-        mnuMain.add(mnuAbout);
-        mnuMain.add(mnuExit);//---->Menu Bar Complete
+        mnuMain.add(mnuExit);
 
-//Adding buttons to NewGame panel
+        //добавление кнопок на панель для новой игры
         pnlNewGame.add(btn1v1);
-        pnlNewGame.add(btn1vCPU);
 
-//Adding Action Listener to all the Buttons and Menu Items
+        //Добавление слушателей для кнопок и пунктов меню
         mnuNewGame.addActionListener(this);
         mnuExit.addActionListener(this);
-        mnuInstruction.addActionListener(this);
-        mnuAbout.addActionListener(this);
         btn1v1.addActionListener(this);
-        btn1vCPU.addActionListener(this);
         btnBack.addActionListener(this);
 
-//Setting up the playing field
+        //настройка игрового поля
         pnlPlayingField.setLayout(new GridLayout(3, 3, 2, 2));
         pnlPlayingField.setBackground(Color.black);
         for(int i=1; i<=9; i++) {
@@ -93,17 +85,18 @@ public class TicTacToe implements ActionListener {
             btnEmpty[i].addActionListener(this);
             pnlPlayingField.add(btnEmpty[i]);
         }
-//Adding everything needed to pnlNorth and pnlSouth
+
+        //добавление панелей
         pnlNorth.add(mnuMain);
         pnlSouth.add(lblTitle);
 
-//Adding to window and Showing window
+        //добавление окон
         window.add(pnlNorth, BorderLayout.NORTH);
         window.add(pnlSouth, BorderLayout.CENTER);
         window.setVisible(true);
     }
 
-    //-------------------START OF ACTION PERFORMED CLASS-------------------------//
+
     public void actionPerformed(ActionEvent click) {
         Object source = click.getSource();
         for(int i=1; i<=9; i++) {
@@ -133,10 +126,10 @@ public class TicTacToe implements ActionListener {
         }
         else if(source == btn1v1) {
             if(inGame) {
-                int option = JOptionPane.showConfirmDialog(null, "If you start a new game," +
-                        "your current game will be lost..." + "\n" +
-                        "Are you sure you want to continue?",
-                        "Quit Game?" ,JOptionPane.YES_NO_OPTION);
+                int option = JOptionPane.showConfirmDialog(null, "Если вы начнёте новую игру" +
+                        "то ваша текущая игра будет потеряна..." + "\n" +
+                        "Вы хотите продолжить?",
+                        "Завершить игру?" ,JOptionPane.YES_NO_OPTION);
                 if(option == JOptionPane.YES_OPTION) {
                     inGame = false;
                 }
@@ -155,36 +148,11 @@ public class TicTacToe implements ActionListener {
 
             }
         }
-        else if(source == btn1vCPU) {
-            JOptionPane.showMessageDialog(null, "Coming Soon!!");
-        }
         else if(source == mnuExit) {
-            int option = JOptionPane.showConfirmDialog(null, "Are you sure you want to exit?",
-                    "Exit Game" ,JOptionPane.YES_NO_OPTION);
+            int option = JOptionPane.showConfirmDialog(null, "Вы уверенны что хотите выйти?",
+                    "Завершить игру" ,JOptionPane.YES_NO_OPTION);
             if(option == JOptionPane.YES_OPTION)
                 System.exit(0);
-        }
-        else if(source == mnuInstruction || source == mnuAbout) {
-            clearPanelSouth();
-            String message = "";
-            txtMessage.setBackground(new Color(color, color, color));
-            if(source == mnuInstruction) {
-                message = "Instructions:\n\n" +
-                        "Your goal is to be the first player to get 3 X's or O's in a\n" +
-                        "row. (horizontally, diagonally, or vertically)";
-            } else {
-                message = "About:\n\n" +
-                        "Title: Tic-Tac-Toe\n" +
-                        "Author: Blmaster\n" +
-                        "Version: " + VERSION + "\n";
-            }
-            txtMessage.setEditable(false);
-            txtMessage.setText(message);
-            pnlSouth.setLayout(new GridLayout(2, 1, 2, 5));
-            pnlTop.add(txtMessage);
-            pnlBottom.add(btnBack);
-            pnlSouth.add(pnlTop);
-            pnlSouth.add(pnlBottom);
         }
         else if(source == btnBack) {
             if(inGame)
@@ -199,15 +167,9 @@ public class TicTacToe implements ActionListener {
         pnlSouth.setVisible(false);
         pnlSouth.setVisible(true);
     }
-//-------------------END OF ACTION PERFORMED CLASS-------------------------//
 
-    /*
-    ----------------------------------
-    Start of all the other methods. |
-    ----------------------------------
-    */
-    public void showGame() { // Shows the Playing Field
-// *IMPORTANT*- Does not start out brand new (meaning just shows what it had before)
+    //показывать игровые поля
+    public void showGame() {
         clearPanelSouth();
         inGame = true;
         pnlSouth.setLayout(new BorderLayout());
@@ -215,26 +177,14 @@ public class TicTacToe implements ActionListener {
         pnlPlayingField.requestFocus();
     }
 
-    public void checkWin() { // checks if there are 3 symbols in a row vertically, diagonally, or horizontally.
-// then shows a message and disables buttons.
+    //проверка на выиграшную комбинацию
+    public void checkWin() {
         for(int i=0; i<7; i++) {
             if(
                     !btnEmpty[winCombo[i][0]].getText().equals("") &&
                             btnEmpty[winCombo[i][0]].getText().equals(btnEmpty[winCombo[i][1]].getText()) &&
-// if {1 == 2 && 2 == 3}
                             btnEmpty[winCombo[i][1]].getText().equals(btnEmpty[winCombo[i][2]].getText())
-/*
-The way this checks the if someone won is:
-First: it checks if the btnEmpty[x] is not equal to an empty string- x being the array number
-inside the multi-dementional array winCombo[checks inside each of the 7 sets][the first number]
-Secong: it checks if btnEmpty[x] is equal to btnEmpty[y]- x being winCombo[each set][the first number]
-y being winCombo[each set the same as x][the second number] (So basically checks if the first and
-second number in each set is equal to each other)
-Third: it checks if btnEmtpy[y] is eual to btnEmpty[z]- y being the same y as last time and z being
-winCombo[each set as y][the third number]
-Conclusion: So basically it checks if it is equal to the btnEmpty is equal to each set of numbers
-*/
-                    ) {
+                ) {
                 win = true;
                 wonNumber1 = winCombo[i][0];
                 wonNumber2 = winCombo[i][1];
@@ -248,12 +198,12 @@ Conclusion: So basically it checks if it is equal to the btnEmpty is equal to ea
         if(win || (!win && turn>9)) {
             if(win) {
                 if(turn % 2 == 0)
-                    message = "X has won!";
+                    message = "X выиграл!";
                 else
-                    message = "O has won!";
+                    message = "O выиграл!";
                 win = false;
             } else if(!win && turn>9) {
-                message = "Both players have tied!\nBetter luck next time.";
+                message = "Оба игрока зашли в тупик!\nСыграйте ещё.";
             }
             JOptionPane.showMessageDialog(null, message);
             for(int i=1; i<=9; i++) {
@@ -262,9 +212,8 @@ Conclusion: So basically it checks if it is equal to the btnEmpty is equal to ea
         }
     }
 
-    public void clearPanelSouth() { //Removes all the possible panels
-//that pnlSouth, pnlTop, pnlBottom
-//could have.
+    //убирает все доступные панели   которые только могут быть
+    public void clearPanelSouth() {
         pnlSouth.remove(lblTitle);
         pnlSouth.remove(pnlTop);
         pnlSouth.remove(pnlBottom);
@@ -279,12 +228,3 @@ Conclusion: So basically it checks if it is equal to the btnEmpty is equal to ea
 
     }
 }
-/*
-Changes:
-1.0- changes below: Stable
-0.9- added back button, added comments.
-0.8- added dynamic win message.
-0.7- added game function- game playable.
-0.6- changed menu layout.
-0.5- basic functions with menu and nice GUI.
-*/
